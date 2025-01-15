f936cf235d3e2c277da393da6e01f77fcef65d19
Aaron Jevon Benedict Kongdoh <abenedict01@student.ciputra.ac.id>
2025-01-15 23:13:02 +0800

    minor update

--- function.py
+++ function.py
@@ -5,6 +5,7 @@ import random
 from ascii import *
 
 def ls():
+    """Displays a list of files and folders in the current directory."""
     try:
         items = os.listdir()
         for item in items:
@@ -13,12 +14,14 @@ def ls():
         print(f"Error: {e}")
 
 def pwd():
+    """Displays the current working directory."""
     try:
         print(os.getcwd())
     except Exception as e:
         print(f"Error: {e}")
 
 def cd(path):
+    """Change working directory."""
     try:
         os.chdir(path)
         print(f"Changed directory to {os.getcwd()}")
@@ -26,6 +29,7 @@ def cd(path):
         print(f"Error: {e}")
 
 def mkdir(directory_name):
+    """Create a new directory."""
     try:
         os.mkdir(directory_name)
         print(f"Directory '{directory_name}' created successfully.")
@@ -33,6 +37,7 @@ def mkdir(directory_name):
         print(f"Error: {e}")
 
 def rmdir(directory_name):
+    """Delete directory (if empty)."""
     try:
         os.rmdir(directory_name)
         print(f"Directory '{directory_name}' removed successfully.")
@@ -40,6 +45,7 @@ def rmdir(directory_name):
         print(f"Error: {e}")
 
 def touch(file_name):
+    """Create a new empty file."""
     try:
         with open(file_name, 'w') as file:
             pass
@@ -48,6 +54,7 @@ def touch(file_name):
         print(f"Error: {e}")
 
 def rm(file_name):
+    """Delete files."""
     try:
         os.remove(file_name)
         print(f"File '{file_name}' removed successfully.")
@@ -55,6 +62,7 @@ def rm(file_name):
         print(f"Error: {e}")
 
 def cp(source, destination):
+    """Copy files from one location to another."""
     try:
         shutil.copy(source, destination)
         print(f"File copied from '{source}' to '{destination}'.")
@@ -62,6 +70,7 @@ def cp(source, destination):
         print(f"Error: {e}")
 
 def mv(source, destination):
+    """Moving or renaming files/directories."""
     try:
         shutil.move(source, destination)
         print(f"Moved or renamed from '{source}' to '{destination}'.")
@@ -69,12 +78,14 @@ def mv(source, destination):
         print(f"Error: {e}")
 
 def clear():
+    """Clearing the terminal screen."""
     try:
         os.system('cls' if os.name == 'nt' else 'clear')
     except Exception as e:
         print(f"Error: {e}")
 
 def exit():
+    """Exit from Clinese"""
     try:
         clear()
         print(xijinping)
@@ -99,7 +110,7 @@ def help():
     exit: Exit from Clinese
 
 Extra Command List:
-    tree: Display directory tree.
+    tree: Display the directory structure as a tree.
     quotes: Display most impactful Chinese Quotes.
     hongbao: Gacha ampao.
     ''')
@@ -119,6 +130,7 @@ def tree(path=".", level=0):
         print(f"Error: {e}")
 
 def quotes():
+    """Display most impactful Chinese Quotes."""
     quotes = [
         'Tuntutlah ilmu sampai ke negeri China',
         '千里之行，始于足下。',
@@ -126,11 +138,14 @@ def quotes():
         '活到老，学到老。',
         '时间就是金钱。',
         '与其临渊羡鱼，不如退而结网。',
+        '水滴石穿。',
+        '塞翁失马，焉知非福。',
         '气贯长虹!',
         'Gōngxǐ fācái'
         ]
     suntzu(random.choice(quotes))
 
 def hongbao():
-    uang = [qr, seratus, limpul, dupul, sepuluh, zonk]
+    """Gacha ampao."""
+    uang = [qr, limratus, seratus, limpul, dupul, sepuluh, zonk]
     print(random.choice(uang))
